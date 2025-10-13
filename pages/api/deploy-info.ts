40637fd513716674f66692748baf781a478f436f
serdchef <a.serdarcarl@gmail.com>
2025-10-13 18:19:43 +0300

    chore(deploy-info): update commit to latest HEAD (4620094)

--- pages/api/deploy-info.ts
+++ pages/api/deploy-info.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // Hard-coded to the current HEAD commit at the time of creating this file.
   // Update this file or check its value after deploy to confirm Vercel is using the latest commit.
-  const commit = '62d4f20'
+  const commit = '4620094'
   const now = new Date().toISOString()
 
   res.status(200).json({ commit, now })
